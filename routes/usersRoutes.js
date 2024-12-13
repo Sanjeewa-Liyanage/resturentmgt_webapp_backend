f29709f489334370b8c7528a544d5069c1a62208
Sanjeewa Liyanage <138761214+Sanjeewa-Liyanage@users.noreply.github.com>
2024-12-14 01:03:44 +0530

    added three functions
    added three functions to get banned users all users and active users

--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -1,5 +1,5 @@
 import express from  'express';
-import { disableUser, enableUser, getUser, loginUser, postUsers } from '../controllers/userController.js';
+import { disableUser, enableUser, getActiveUsers, getAllUsers, getBannedUsers, getUser, loginUser, postUsers } from '../controllers/userController.js';
 
 const userRouter = express.Router();
 
@@ -9,7 +9,11 @@ userRouter.post("/",postUsers)
 userRouter.post("/login",loginUser)
 userRouter.put("/enable/:userId",enableUser)
 userRouter.put("/disable/:userId",disableUser)
+userRouter.get("/all",getAllUsers)
+userRouter.get("/banned",getBannedUsers)
+userRouter.get("/active",getActiveUsers)
 userRouter.get("/",getUser)
+
 
 
 
